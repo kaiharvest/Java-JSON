0b5fca5167e547f11e4da386f450cbdbaba2fb5b
kaiharvest <indradwiprabowo27@gmail.com>
2026-01-06 23:07:32 +0700

    Read JSON Object

--- src/test/java/indradwiprabowo/json/JsonObjectTest.java
+++ src/test/java/indradwiprabowo/json/JsonObjectTest.java
@@ -1,9 +1,12 @@
 package indradwiprabowo.json;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 import tools.jackson.databind.exc.JsonNodeException;
 
+import java.lang.reflect.Type;
 import java.util.Map;
 
 public class JsonObjectTest {
@@ -26,6 +29,30 @@ public class JsonObjectTest {
         String json = objectMapper.writeValueAsString(map);
 
         System.out.println(json);
+    }
+
+    @Test
+    void readJson() throws JsonNodeException {
+        String json = """
+                {
+                    "firstName": "Budi",
+                    "address": {
+                        "city": "Semarang",
+                        "streat": "belum ada",
+                        "country": "Indonesia"
+                    },
+                    "age": 30,
+                    "married": true,
+                    "lastName": "Nugraha"
+                }
+                """;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> person = objectMapper.readValue(json, new TypeReference<Map<String, Object>>() {
+        });
+
+        Assertions.assertEquals("Budi", person.get("firstName"));
+        Assertions.assertEquals("Nugraha", person.get("lastName"));
     }
 
 }
